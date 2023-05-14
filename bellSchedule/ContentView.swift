6068c256f7d1b205377e3f5537356e0cf18d2cc6
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2023-05-13 17:26:52 -0700

    Initial Commit

---
+++ bellSchedule/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  bellSchedule
+//
+//  Created by Ari Stassinopoulos on 2023-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bellSchedule/bellScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  bellScheduleApp.swift
+//  bellSchedule
+//
+//  Created by Ari Stassinopoulos on 2023-05-13.
+//
+
+import SwiftUI
+
+@main
+struct bellScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
